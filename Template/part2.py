b3d0fb8cf4df892f85ab37f137eb90d5ab25b406
Keith Beja <keith.beja@xppower.com>
2021-12-02 19:04:15 +0800

    Add template

---
+++ Template/part2.py
@@ -0,0 +1,12 @@
+from solver import Solver
+
+
+class Part2(Solver):
+    def __init__(self):
+        pass
+
+    def process(self, input: str):
+        pass
+
+    def finish(self):
+        pass
